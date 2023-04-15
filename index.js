9a5f89f01639c945973d2df2c3a8b275180205ee
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-04-15 17:31:54 -0400

    Devolviendo mensaje y cors

--- index.js
+++ index.js
@@ -12,7 +12,11 @@ const app = express();
 const server = http.createServer(app);
 
 //* Nueva instancia de servidor socket.io
-const socket = new SocketServer(server);
+const socket = new SocketServer(server, {
+    cors: {
+        origin: ["http://localhost:5173", "http://192.168.15.171:5173"],
+    },
+});
 
 //* Utilización de cors
 app.use(cors());
@@ -26,8 +30,18 @@ dotenv.config();
 //* Puerto del server, sino hay puerto en .env coloca el puerto 5000
 const port = process.env.PORT ?? "5000";
 
+//* Escucha de socket.io sobre la conexión
+socket.on("connection", (socket) => {
+    socket.on("message", (mensaje) => {
+        socket.broadcast.emit("message", {
+            body: mensaje,
+            from: socket.id,
+        });
+    });
+});
+
 //* Levanta el server con el puerto definido
-app.listen(port);
+server.listen(port);
 
 //* Mensaje de ejecución
 console.log(`Server funcionando por el puerto ${port}`);
